2c1cdd10a14cfaff44fd896f85dcc524daeadabf
Shefer Alexander <shefard55r@gmail.com>
2019-11-08 12:06:55 +0300

    Add project files.

---
+++ ConsoleOutputEngine.Core/Models/Pixel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleOutputEngine.Core.Models
+{
+    public class Pixel
+    {
+        public int PosX { get; private set; }
+        public int PosY { get; private set; }
+        public char Value { get; private set; }
+    }
+}
---
+++ ConsoleOutputEngine/Engine.cs
@@ -0,0 +1,91 @@
+﻿using ConsoleOutputEngine.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace ConsoleOutputEngine
+{
+    public class Engine : IDisposable
+    {
+        private Process _process;
+        private int _pixelThicc = 2;
+        
+        public char WhitePixelValue { get; }
+
+        public Engine(): this(' ')
+        {
+            
+        }
+
+        public Engine(char whitePixelValue)
+        {
+            WhitePixelValue = whitePixelValue;
+
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd.exe")
+            {
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+
+            _process = Process.Start(procStartInfo);
+
+            Console.SetOut(_process.StandardInput);
+        }
+
+        public void Render(IEnumerable<Pixel> pixelMap)
+        {
+            var pixelMapOrdered = pixelMap
+                .OrderBy(x => x.PosX)
+                .GroupBy(x => x.PosY)
+                .OrderBy(g => g.Key);
+
+            var currentPosX = 0;
+            var currentPosY = 0;
+
+            Clear();
+
+            foreach (var pixelLine in pixelMapOrdered)
+            {
+                while (currentPosY < pixelLine.Key)
+                {
+                    Console.WriteLine();
+                }
+
+                foreach (var pixel in pixelLine)
+                {
+                    while (currentPosX < pixel.PosX)
+                    {
+                        RenderPixel(WhitePixelValue);
+                    }
+
+                    RenderPixel(pixel.Value);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        public void Clear()
+        {
+            Console.Clear();
+        }
+
+        private void RenderPixel(char pixelValue)
+        {
+            for (int i = 0; i < _pixelThicc; i++)
+            {
+                Console.Write(pixelValue);
+            }
+        }
+
+        public void Dispose()
+        {
+            _process.Dispose();
+        }
+    }
+}
